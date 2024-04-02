5358877752fd6e0f1653127481d231ab34e16f13
mehdi <mehdinazeer0@gmail.com>
2024-04-02 05:46:46 +0500

    first commit

---
+++ Assignment3/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Assignment#3
+//storing the famous qoute and its author
+let quote = "Nothing is impossible because impossible it self I M POSSIBLE";
+let author = "Mehdi";
+//printing the message
+console.log('$ {quote}"- ${author}');
---
+++ Assignment3/main.ts
@@ -0,0 +1,9 @@
+//Assignment#3
+
+//storing the famous qoute and its author
+
+let quote : string = "Nothing is impossible because impossible it self I M POSSIBLE"
+
+let author : string = "Mehdi";
+//printing the message
+console.log('$ {quote}"- ${author}');
